cf3a659129316211ded88d024a346e4f8c129a4b
Richard Chernanko <rchernanko@hotmail.co.uk>
2015-09-30 10:31:30 +0100

    initial commit. adding .gitignore file, new pom.xml and my first junit test class

---
+++ src/test/java/com/richard/selenium/MyFirstUnitTest.java
@@ -0,0 +1,12 @@
+package com.richard.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyFirstUnitTest {
+
+    @Test
+    public void myFirstUnitTest() {
+        Assert.assertTrue(true);
+    }
+}
