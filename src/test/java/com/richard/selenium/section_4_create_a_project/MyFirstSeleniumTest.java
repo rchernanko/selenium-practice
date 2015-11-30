e08d443c9a6dbfea3ecdde2290219380d2bd3f71
Richard Chernanko <rchernanko@hotmail.co.uk>
2015-11-30 15:01:20 +0000

    adding comment relating to webdriver interface

--- src/test/java/com/richard/selenium/section_4_create_a_project/MyFirstSeleniumTest.java
+++ src/test/java/com/richard/selenium/section_4_create_a_project/MyFirstSeleniumTest.java
@@ -24,9 +24,19 @@ public class MyFirstSeleniumTest {
         //HtmlUnitDriver driver = new HtmlUnitDriver();
 
         //Why use the interface then?
-        //Well we may want to inject, in our tests, the driver, and have the driver itself instantiated somewhere else because
-        //we may want our test to run across numerous different browsers (firefox, chrome etc).
-        //Need to ask the others about this...I don't quite understand...
+        //Well we may want to inject, in our tests, the driver, and have the driver itself instantiated somewhere else
+        //because we may want our test to run across numerous different browsers (firefox, chrome etc).
+
+        //Above - we use the Webdriver interface in the shopping-test-framework (and Magentys core). WebDriver
+        //(interface) allows us to build one page object / one test (with a web driver variable) that we can then pass
+        //in a web driver. Basically allows you to run the same test on a given page object in chrome, in firefox, in ie
+        //(depending on the driver you pass into it). If we didn't use the webdriver interface (and instead used e.g
+        //ChromeDriver driver = new ChromeDriver()), then that test can only run in chromedriver.
+        //Using the webdriver interface gives us more flexibility to run the same test (using the same page object) on
+        //multiple browsers.
+
+        //Good photo example in my evernote from Marc - Webdriver interface example
+
         //some more info - https://code.google.com/p/selenium/wiki/NextSteps
         //https://code.google.com/p/selenium/wiki/GettingStarted
         //reference - 46 - My first test explored
