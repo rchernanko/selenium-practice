a185e369dd2f801ec94efec9824302d543fa04c0
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-04-29 16:29:10 +0100

    amending pom file to not include marionette driver as was causing me headaches. instead using firefox driver on older version of firefox. but will upgrade in the future

--- src/test/java/com/richard/selenium/section_4_create_a_project/No2_GetPageGetTitleTest.java
+++ src/test/java/com/richard/selenium/section_4_create_a_project/No2_GetPageGetTitleTest.java
@@ -10,24 +10,6 @@ import org.openqa.selenium.firefox.MarionetteDriver;
 public class No2_GetPageGetTitleTest {
 
     @Test
-    public void isBbcFootballWebsiteTitleCorrect() {
-
-        /*
-
-        If using Firefox Version 45 or below, I can use FirefoxDriver()
-
-        However, if I am testing on Firefox Version 46 or above, I need to use another driver entitled
-        'Marionette' - see readme
-         */
-
-        WebDriver driver = new MarionetteDriver();
-        driver.get("http://www.bbc.co.uk/football/");
-        String pageTitle = driver.getTitle();
-        Assert.assertTrue("The page title is incorrect", pageTitle.startsWith("Football"));
-        driver.close();
-    }
-
-    @Test
     public void isArsenalWebsiteTitleCorrect() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.arsenal.com/");
@@ -35,19 +17,33 @@ public class No2_GetPageGetTitleTest {
         driver.close();
     }
 
-    //Using FirefoxDriver, ChromeDriver etc take longer to execute compared to the headless browser driver HtmlUnitDriver
-    //Also, in the above, once the test has run, we are still left with a browser on the screen - this is bad!
-    //This will be a killer if we have more tests!
-    //So to close the browser windows, we can either use driver.close() or driver.quit()
+    /*
+    Using FirefoxDriver, ChromeDriver etc take longer to execute compared to the headless browser driver HtmlUnitDriver
 
-    //driver.quit() will close all of the windows that are open (within your specific junit test)
-    //driver.close() will close only the window that is open
-    //My guess is that 9 times out of 10, you'd want to use the quit() method
+    Also, in the above, once the test has run, we are still left with a browser on the screen - this is bad!
+    This will be a killer if we have more tests!
+    So to close the browser windows, we can either use driver.close() or driver.quit()
 
-    //Of course, you don't have to do this with HtmlUnitDriver because it is a headless driver (and so doesn't run in a browser)
+    driver.quit() will close all of the windows that are open
+    driver.close() will close only the window that is open
+
+    Of course, you don't have to do this with HtmlUnitDriver because it is a headless driver (and so doesn't run in a browser)
+
+    */
 
     @Test
     public void isChannelFourWebsiteTitleCorrect() {
+
+        /*
+
+        If using Firefox Version 45 or below, I can use FirefoxDriver()
+
+        However, if I am testing on Firefox Version 46 or above, I need to use another driver entitled
+        'Marionette' - see readme
+
+        For now, I will leave my firefox version at 45 for the intent of learning more webdriver quicker
+
+         */
 
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.channel4.com/");
@@ -64,8 +60,11 @@ public class No2_GetPageGetTitleTest {
         driver.close();
     }
 
-    //Of course, the above driver only closes if the test passes (i.e. it gets to a point where the JVM can execute
-    //the driver.close() method.
-    //If there is an error in the assert, the assertTrue method will throw an error and driver.close won't be executed
+    /*
 
+    Of course, the above driver only closes if the test passes (i.e. it gets to a point where the JVM can execute
+    the driver.close() method.
+    If there is an error in the assert, the assertTrue method will throw an error and driver.close won't be executed
+
+    */
 }
