9235cd038d6d95d9fa5a9eaf93e956524e594114
rchernanko <rchernanko@hotmail.co.uk>
2016-05-04 22:12:18 +0100

    formatting changes and also adding comments

--- src/test/java/com/richard/selenium/section_10_interrogation/No1_Driver_InterrogationTest.java
+++ src/test/java/com/richard/selenium/section_10_interrogation/No1_Driver_InterrogationTest.java
@@ -12,6 +12,21 @@ He splits this into 2 types:
 
 2) Interrogation of the DOM
 
+    - In order to interrogate, you have to locate the DOM element (WebElement):
+        - findElement()
+        - findElements()
+
+    - Once located, you can then interrogate using the WebElement Object methods:
+        - getText()
+        - getAttribute()
+        - getTagName()
+        - isEnabled()
+        - isSelected()
+        - isDisplayed()
+        - getSize()
+        - getLocation()
+        - getCssValue()
+
  */
 
 package com.richard.selenium.section_10_interrogation;
@@ -22,36 +37,37 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import static org.hamcrest.CoreMatchers.*;
 
 public class No1_Driver_InterrogationTest {
 
     //Let's write some tests for interrogation of the driver (point 1 in comments above)
 
-	private WebDriver driver;
-	private static final String BASE_URL = "http://www.compendiumdev.co.uk/selenium/basic_web_page.html";
+    private WebDriver driver;
+    private static final String BASE_URL = "http://www.compendiumdev.co.uk/selenium/basic_web_page.html";
 
-	@Before
-	public void createDriverInstance() {
-		driver = new FirefoxDriver();
-	}
+    @Before
+    public void createDriverInstance() {
+        driver = new FirefoxDriver();
+    }
 
     @Test
     public void driverLevelInterrogation() {
 
-		//Navigate to the page and assert the page title
-		driver.get(BASE_URL);
-		Assert.assertThat("Page title is incorrect", driver.getTitle(), is("Basic Web Page Title"));
+        //Navigate to the page and assert the page title
+        driver.get(BASE_URL);
+        Assert.assertThat("Page title is incorrect", driver.getTitle(), is("Basic Web Page Title"));
 
-		//assert current url
-		Assert.assertTrue("Current url is incorrect", (driver.getCurrentUrl().equals(BASE_URL)));
-		//Could've also done:
-		//Assert.assertEquals("Current url is incorrect", driver.getCurrentUrl(), BASE_URL);
+        //assert current url
+        Assert.assertTrue("Current url is incorrect", (driver.getCurrentUrl().equals(BASE_URL)));
+        //Could've also done:
+        //Assert.assertEquals("Current url is incorrect", driver.getCurrentUrl(), BASE_URL);
 
-		//assert page source contains specific text
-		Assert.assertThat("Page source does not contain specific text", driver.getPageSource(), containsString("A paragraph of text"));
-		//Could've also done:
-		//Assert.assertTrue(driver.getPageSource().contains("A paragraph of text"));
+        //assert page source contains specific text
+        Assert.assertThat("Page source does not contain specific text", driver.getPageSource(), containsString("A paragraph of text"));
+        //Could've also done:
+        //Assert.assertTrue(driver.getPageSource().contains("A paragraph of text"));
 
 		/*
 
@@ -65,12 +81,9 @@ public class No1_Driver_InterrogationTest {
 
 		*/
 
-	}
+    }
 
-	@After
-	public void quitDriver() {
-		driver.quit();
-	}
-
-	//Up to lesson 71
-}
+    @After
+    public void quitDriver() {
+        driver.quit();
+    }}
