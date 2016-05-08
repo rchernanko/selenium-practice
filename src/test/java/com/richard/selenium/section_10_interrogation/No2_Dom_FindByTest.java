0f4784fed757b84c4632d0f97f3aa799ff1353e3
rchernanko <rchernanko@hotmail.co.uk>
2016-05-08 16:49:59 +0100

    updating the interrogation tests

--- src/test/java/com/richard/selenium/section_10_interrogation/No2_Dom_FindByTest.java
+++ src/test/java/com/richard/selenium/section_10_interrogation/No2_Dom_FindByTest.java
@@ -31,6 +31,16 @@ He splits this into 2 types:
 
 package com.richard.selenium.section_10_interrogation;
 
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.fail;
+
 public class No2_Dom_FindByTest {
 
     /*
@@ -51,6 +61,116 @@ public class No2_Dom_FindByTest {
     - By.tagName
     - By.partialLinkText
 
+    Let's write some tests for interrogation of the dom (point 2 in comments above)
+
      */
 
+    private static WebDriver driver;
+    private static final String BASE_URL = "http://www.compendiumdev.co.uk";
+
+    @BeforeClass
+    public static void createDriverInstanceAndNavigateToPage() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL + "/selenium/find_by_playground.php");
+    }
+
+    @Test
+    public void findElementByIdTest() {
+        WebElement element = driver.findElement(By.id("a47"));
+        Assert.assertThat("Element text is not correct", element.getText(), is("jump to para 21"));
+    }
+
+    @Test //TODO xpath
+    public void findElementByXpathTest() {
+    }
+
+    @Test
+    public void findElementByCssSelectorTest() {
+        WebElement element = driver.findElement(By.cssSelector(".nestedDiv #p35"));
+        Assert.assertTrue("WebElement's attribute is not correct", element.getAttribute("name").equals("pName35"));
+    }
+
+    @Test
+    public void findElementByClassNameTest() {
+        WebElement element = driver.findElement(By.className("specialDiv"));
+        Assert.assertEquals("Element text is not correct", element.getAttribute("name"), "mydivname");
+    }
+
+    @Test
+    public void findElementByLinkTextTest() {
+        WebElement element = driver.findElement(By.linkText("jump to para 23"));
+        Assert.assertThat(element.getText(), is("jump to para 23"));
+    }
+
+    @Test
+    public void findElementByNameTest() {
+        WebElement element = driver.findElement(By.name("pName26"));
+        Assert.assertTrue("WebElement's id is not correct", element.getAttribute("id").equals("p26"));
+    }
+
+    @Test //TODO ByTagName
+    public void findElementByTagNameTest() {
+        WebElement element = driver.findElement(By.tagName(""));
+        Assert.assertThat("", true, is(true));
+    }
+
+    @Test //TODO getTagName
+    public void findElementByPartialLinkTextTest() {
+        WebElement element = driver.findElement(By.partialLinkText("para 12"));
+        Assert.assertEquals(element.getTagName(), "sdfsd");
+    }
+
+    @Test
+    public void isElementDisplayedTest() {
+        WebElement element = driver.findElement(By.cssSelector("#li16"));
+        Assert.assertTrue("The WebElement is not displayed", element.isDisplayed());
+    }
+
+    @Test //TODO assertIsEnabled
+    public void isElementSelectedTest() {
+        WebElement element = driver.findElement(By.cssSelector("#li16"));
+    }
+
+    //few more to do as well - check the top TODOS
+
+    /*
+
+    - getLocation() TODO
+    - getCssValue() TODO
+
+    */
+
+    /*
+
+    Below are some tests that validate that a NoSuchElementException is thrown
+    There are 2 ways to do this, either with a try/catch or with an expected next to the @Test annotation
+    Alan prefers the try/catch because it is a bit more flexible
+
+     */
+
+    @Test
+    public void tryCatchNoSuchElementExceptionTest() {
+
+        try {
+            WebElement element = driver.findElement(By.className("richard"));
+
+            //If the above does not thrown a NoSuchElementException, then the line below will be executed and will fail
+            //the test
+            fail("Expected a NoSuchElementException to be thrown");
+
+        } catch (NoSuchElementException e) {
+            //Make sure I'm using the Selenium API NoSuchElementException and not the junit one! Be careful!!!
+            System.out.println("NoSuchElementException is correctly thrown");
+        }
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void junitExpectedNoSuchElementExceptionTest() {
+        WebElement element = driver.findElement(By.id("richard"));
+    }
+
+    @AfterClass
+    public static void quitDriverInstance() {
+        driver.quit();
+    }
 }
