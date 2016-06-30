bbe3139eff7dad4e7bd6539dc78260b1f6e271d0
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-06-30 09:12:44 +0100

    adding test class with exercise description

---
+++ src/test/java/com/richard/selenium/section_16_user_interactions/No2_User_Interaction_Exercises_Test.java
@@ -0,0 +1,21 @@
+package com.richard.selenium.section_16_user_interactions;
+
+public class No2_User_Interaction_Exercises_Test {
+
+    /*
+
+    Time for some exercises.
+
+    Using http://compendiumdev.co.uk/selenium/gui_user_interactions.html...
+
+    1) Move draggable1 on to droppable1 and assert droppable1 text change to "Dropped!"
+    2) Drag and drop draggable2 to droppable1 and assert droppable1 text change to "Get Off Me!"
+    3) Press control+B and assert for text change on draggable1 to "Bwa! Ha! Ha!"
+
+    Optional challenages:
+
+    1) Can you draw something in the canvas?
+    2) Control+Space and red squares say "Let GO!!" Can you assert for this?
+
+     */
+}
