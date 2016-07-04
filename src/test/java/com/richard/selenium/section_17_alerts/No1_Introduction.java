6ca5f06ca7ae829086a7da95e48248d83d64dc22
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-07-04 18:17:57 +0100

    adding introduction to alerts

---
+++ src/test/java/com/richard/selenium/section_17_alerts/No1_Introduction.java
@@ -0,0 +1,29 @@
+package com.richard.selenium.section_17_alerts;
+
+public class No1_Introduction {
+
+/*
+
+- People sometimes get confused by what an alert actually is. An alert is something that is triggered by JavaScript alert code, for example:
+
+alert('hello there');
+prompt('this is a prompt')
+
+- An alert is NOT a modal / lightbox. Sometimes people tend to get confused with these 2 very separate things
+
+Handling alerts:
+
+driver.switchTo().alert()...
+
+this switches you from the main window to the alert. .alert() returns an Alert object, which you can then do the following with:
+
+- .getText()
+- .dismiss()
+- .accept()
+- .sendKeys(String)
+
+Is pretty straightforward.
+
+ */
+
+}
