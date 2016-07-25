d77006bf60f24d023879f321a63a27643f884d5b
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-07-25 11:03:44 +0100

    adding first javascript exercise

--- src/test/java/com/richard/selenium/section_27_javascript/No2_Javascript_Executor_Exercises_Test.java
+++ src/test/java/com/richard/selenium/section_27_javascript/No2_Javascript_Executor_Exercises_Test.java
@@ -1,5 +1,13 @@
 package com.richard.selenium.section_27_javascript;
 
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
 public class No2_Javascript_Executor_Exercises_Test {
 
     /*
@@ -21,8 +29,42 @@ public class No2_Javascript_Executor_Exercises_Test {
 
      */
 
+    private static WebDriver driver;
 
+    @BeforeClass
+    public static void setUp() {
+        driver = new ChromeDriver();
+        driver.get("http://www.compendiumdev.co.uk/selenium/canvas_basic.html");
+    }
 
+    @Before
+    public void resetBeforeTest() {
+        driver.navigate().refresh(); //set's the javascript back to the default before each test :-)
+    }
 
+    @Test
+    public void exercise1PassArgumentsToTheJavaScript() {
 
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        int actionsCount = driver.findElements(By.cssSelector("#commandlist li")).size();
+        assertEquals(2, actionsCount);
+
+        //Now I am going to execute some JavaScript (within a for loop) with arguments that I am passing in
+        for (int testLoop=0; testLoop < 10; testLoop++) {
+
+            js.executeScript("draw(0, arguments[0], arguments[1], 20, arguments[2]);",
+                    testLoop*20,
+                    testLoop*20,
+                    "#" + testLoop  + testLoop + "0000");
+        }
+
+        actionsCount = driver.findElements(By.cssSelector("#commandlist li")).size();
+        assertEquals(12, actionsCount);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
 }
