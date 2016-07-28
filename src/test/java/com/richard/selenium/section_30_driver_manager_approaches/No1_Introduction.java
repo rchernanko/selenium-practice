722b8680f28a25fe48f4049159a91b6dfc8c412d
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-07-28 15:52:16 +0100

    adding driver manager introduction class - nothing in it though, just comments

---
+++ src/test/java/com/richard/selenium/section_30_driver_manager_approaches/No1_Introduction.java
@@ -0,0 +1,19 @@
+package com.richard.selenium.section_30_driver_manager_approaches;
+
+public class No1_Introduction {
+
+    /*
+
+    Driver class should help co-ordinate the driver for me :-) I shouldn't have to do a "new ChromeDriver()" every time...
+
+    I've not taken any notes...a lot of the implementations are based on properties files etc - which I've already experimented with...
+
+    If I need to go over any more stuff, just watch the videos again...
+
+    FYI - the below is a more productionised approach to providing web drivers: - TODO - look through:
+
+    https://github.com/SeleniumHQ/selenium/blob/master/java/client/test/org/openqa/selenium/testing/drivers/WebDriverBuilder.java
+
+
+     */
+}
