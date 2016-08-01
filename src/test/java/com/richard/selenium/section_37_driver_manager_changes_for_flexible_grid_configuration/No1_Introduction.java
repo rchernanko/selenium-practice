ee7a2813063e10d8c4c102ee59634878a27d1529
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-08-01 11:41:06 +0100

    adding an introduction to flexible grid management and drivers

---
+++ src/test/java/com/richard/selenium/section_37_driver_manager_changes_for_flexible_grid_configuration/No1_Introduction.java
@@ -0,0 +1,14 @@
+package com.richard.selenium.section_37_driver_manager_changes_for_flexible_grid_configuration;
+
+public class No1_Introduction {
+
+    /*
+
+    These videos looked really good but I've not watched them
+
+    They would probably be really good to watch when / if I needed to set up a selenium grid / SauceLabs from scratch
+
+    TODO - watch!
+
+     */
+}
