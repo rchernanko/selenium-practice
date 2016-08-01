c20c12eaf67b966b1351d364435abb40aa5ab961
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-08-01 09:34:29 +0100

    adding ci introduction + android introduction notes - not much here

---
+++ src/test/java/com/richard/selenium/section_33_android_introduction/No1.java
@@ -0,0 +1,10 @@
+package com.richard.selenium.section_33_android_introduction;
+
+public class No1 {
+
+    /*
+
+    Only 1 video - not taking any notes - will gain experience in new role
+
+    */
+}
---
+++ src/test/java/com/richard/selenium/section_34_ci_introduction/No1_Introduction.java
@@ -0,0 +1,11 @@
+package com.richard.selenium.section_34_ci_introduction;
+
+public class No1_Introduction {
+
+    /*
+
+    Videos showed me how to get jenkins up and running - by downloading it and running it etc.
+    Pretty cool but I won't be taking any notes. If I want more information, I will simply have to re-watch the videos
+
+     */
+}
