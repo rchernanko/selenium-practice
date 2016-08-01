56e70b1a43cb12234cf4985e0e75720cfe40d42d
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-08-01 11:34:45 +0100

    updating the pom with some profiles + updating comments in section_35

--- src/test/java/com/richard/selenium/section_35_ci_junit_suites/No1_Introduction.java
+++ src/test/java/com/richard/selenium/section_35_ci_junit_suites/No1_Introduction.java
@@ -12,5 +12,26 @@ public class No1_Introduction {
 
     To get things started, I have created an example of a basic Junit suite "ExampleSuiteTest" - go here now
 
+    BACK AGAIN!
+
+    So now I've configured some suites to run, I can now add them into my maven profile.
+
+    So i've created 2 profiles in there - the key section is the "include" - here you can see the suites i will run
+    with this profile...
+
+    So if I do a mvn test -Prichardtest = the ExampleSuiteTest.class will be run
+
+    And if I do a mvn test -Prichardanothertest = the AnotherExampleSuiteTest.class will be run
+
+    Pretty simple really - TODO probably still need to figure out what's going on in the shopping-test-framework profiles
+
+
+    OTHER:
+
+    These videos also went through changes made to how we get the webdriver when we run the tests in CI - was quite good
+    but I've not made any changes. This section, for me, is more about configuring the suites to run as part of maven
+    profiles BUT note that there was also some other interesting information in the videos - would be good to watch again
+    if i ever face issues with specific browsers...
+
      */
 }
